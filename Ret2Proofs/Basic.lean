e7533d03970ffbfef22d40284fed067d62dbf51e
Preston Pan <ret2pop@nullring.xyz>
2026-05-27 21:06:05 -0700

    strictly increasing functions injective

--- Ret2Proofs/Basic.lean
+++ Ret2Proofs/Basic.lean
@@ -1,30 +1,33 @@
-import Mathlib.Data.Real.Basic
-import Mathlib.Logic.Function.Basic
-import Mathlib.Tactic.Basic
+import Mathlib
 
--- ====================================================================
--- PROBLEM 1: Order & Logic (Warmup)
--- Prove that a strictly increasing function must be injective (one-to-one).
--- ====================================================================
-
-lemma le_irreflexive (x : ℝ) : ¬ (x < x) := by
-  exact lt_irrefl x
-
-theorem injective_of_strictly_increasing (f : ℝ → ℝ) (h : ∀ x y, x < y → f x < f y) :
+-- Lemma 1: If f(x) is strictly increasing, it is injective.
+-- Proof: suppose f(x) is strictly increasing, yet not injective. Then there exists x, y s.t.
+-- x != y yet f(x) = f(y). WLOG let x < y, then f(x) < f(y), so cannot be equal. Contradiction.
+lemma injective_of_strictly_increasing (f : ℝ → ℝ) (h : ∀ x y, x < y → f x < f y) :
     Function.Injective f := by
-    intro x y
-    -- We want to prove the contrapositive because it is easier
-    contrapose!
-    sorry
+    intro x y h_eq
+    -- Proof by contradiction because it is easier
+    by_contra! h_neq
+    have two_cases : x < y ∨ y < x := lt_or_gt_of_ne h_neq
+    cases two_cases with
+    -- x < y
+    | inl h_lt =>
+      have := h x y h_lt
+      linarith
+    -- y < x
+    | inr h_gt =>
+      have := h y x h_gt
+      linarith
 
--- Proves 2x is a continuous function
-theorem continuous_at_two_x (x₀ : ℝ) :
+-- Lemma 2: f(x) = 2x is a continuous function.
+-- Proof: |x - x₀| < ε/2
+lemma continuous_at_two_x (x₀ : ℝ) :
     ∀ ε > 0, ∃ δ > 0, ∀ x, |x - x₀| < δ → |2 * x - 2 * x₀| < ε := by
   intro ε hε
   use (ε / 2)
   constructor
   · -- Prove (ε / 2) > 0
-    grind_linarith
+    linarith
   · -- Prove the limit implication
     intro x
     rw [← mul_sub 2 x x₀]
@@ -32,15 +35,11 @@ theorem continuous_at_two_x (x₀ : ℝ) :
     norm_num
     grind_linarith
 
--- ====================================================================
--- PROBLEM 3: Epsilon-N Sequence Limits
--- Given a custom definition of a sequence limit, prove that shifting a 
--- convergent sequence by a constant 'c' shifts its limit by 'c'.
--- ====================================================================
-
+-- Use non mathlib construction of a limit for practice.
 def SeqLimit (a : ℕ → ℝ) (L : ℝ) : Prop :=
   ∀ ε > 0, ∃ N : ℕ, ∀ n ≥ N, |a n - L| < ε
 
-theorem seqLimit_add_const (a : ℕ → ℝ) (L c : ℝ) (h : SeqLimit a L) :
+-- Lemma 3: lim sₙ = L => lim sₙ + c = L + c
+lemma seqLimit_add_const (a : ℕ → ℝ) (L c : ℝ) (h : SeqLimit a L) :
     SeqLimit (fun n => a n + c) (L + c) := by
   sorry
